b91e55fc37980db994327a715aba889e1af8c024
Choperchup <pandasleep1810@gmail.com>
2025-09-25 16:53:38 +0700

    fix product

--- routes/web.php
+++ routes/web.php
@@ -12,23 +12,3 @@ Route::prefix('api')->group(function () {
     Route::get('/products', [ProductController::class, 'getProducts'])->name('api.products');
     Route::get('/shops', [ProductController::class, 'getShops'])->name('api.shops');
 });
-
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
-    return view('welcome');
-})->middleware(['verify.shopify'])->name('home');
-
-
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
